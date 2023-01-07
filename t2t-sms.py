05a9d9cd822ac9cdd2cbaa6346582c741f8585f0
Pablo <96752956+pablokbg@users.noreply.github.com>
2023-01-07 17:24:25 +0100

    Update t2t-sms.py

--- t2t-sms.py
+++ t2t-sms.py
@@ -1,31 +1,49 @@
 try:    
     import os
     import requests
-    from flask import Flask, request
+    from flask import Flask, request, abort
     from waitress import serve
+    from functools import wraps
+    from twilio.request_validator import RequestValidator
 except ImportError as err:
     print(f"Failed to import the required modules: {err}")
 
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
 
 webhook = Flask(__name__)
 
+def validate_twilio_request(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        validator = RequestValidator(TWILIO_AUTH_TOKEN)
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        if request_valid:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+
+    return decorated_function
+
 @webhook.route("/", methods=["GET"])
-def handle_get():
-    send_message(f"GET /", TELEGRAM_CHAT_ID)
-    
+def index_webhook():
     return webhook.send_static_file('index.html')
 
 @webhook.route("/sms", methods=["POST"])
+@validate_twilio_request
 def sms_reply():
     message = request.form["Body"]
     sender = request.form["From"]
     
     send_message(f"From: {sender}\n{message}", TELEGRAM_CHAT_ID)
     
-    return webhook.send_static_file('index.html')
+    return 'SUCCESS'
 
 def send_message(message, chat_id):
     url = f"{TELEGRAM_API_URL}/sendMessage?text={message}&chat_id={chat_id}"
